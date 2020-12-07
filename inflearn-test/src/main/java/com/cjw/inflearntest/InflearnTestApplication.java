836a2d42794a18b5fe6adc9fa4cc1d2cae12a778
CJW23 <cjw7242@gmail.com>
2020-12-07 22:21:32 +0900

    자바8 마무리 백기선 자바 테스트 강의 시작

---
+++ inflearn-test/src/main/java/com/cjw/inflearntest/InflearnTestApplication.java
@@ -0,0 +1,13 @@
+package com.cjw.inflearntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnTestApplication.class, args);
+    }
+
+}
---
+++ inflearn-test/src/test/java/com/cjw/inflearntest/InflearnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjw.inflearntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
