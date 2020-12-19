4d41ef793794b74ca30c8c9f94df2505de80672b
CJW23 <cjw7242@gmail.com>
2020-12-19 22:10:30 +0900

    Observer

--- Design_Pattern/src/ObserverTest.java
+++ Design_Pattern/src/ObserverTest.java
@@ -2,41 +2,17 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ObserverTest {
-    public static void main(String[] args) {
-        PlayController playController = new PlayController();
-        Observer ob1 = new MyClassA(playController);
-        Observer ob2 = new MyClassB(playController);
-
-        //playController.setFlag(true);
-        //playController.setFlag(false);
-
-        playController.deleteObserver(ob1);
-        playController.setFlag(false);
-
-
-        //Listener
-        PlayController button = new PlayController();
-        button.setListener(new Observer() {
-            @Override
-            public void update(boolean play) {
-
-            }
-
-            @Override
-            public void onClick() {
-                System.out.println("onClick Listener");
-            }
-        });
-        button.play();
-    }
+//이벤트 발생시키는 객체 인터페이스
+interface Publisher {
+    //관찰자 객체 추가
+    public void addObserver(Observer o);
+    //관찰자 객체 삭제
+    public void deleteObserver(Observer o);
+    //관찰자들에게 이벤트 발생 전달
+    public void notifyObservers();
 }
 
-interface Observer {
-    public void update(boolean play);
-    public void onClick();
-}
-
+//이벤트 객체 구현
 class PlayController implements Publisher {
     private List<Observer> observers = new ArrayList<>();
     private boolean play;
@@ -55,11 +31,13 @@ class PlayController implements Publisher {
     //옵저버들에게 변경사항을 전달
     @Override
     public void notifyObservers() {
+        //옵저버 목록들에게 이벤트 전달
         for (int i=0; i<observers.size(); i++) {
-            observers.get(i).update(play);
+            observers.get(i).notify(play);
         }
     }
 
+    //이벤트 발생 함수
     public void setFlag(boolean play) {
         this.play = play;
         notifyObservers();
@@ -68,42 +46,28 @@ class PlayController implements Publisher {
     public boolean getFlag() {
         return play;
     }
-
-/////Listener처럼 구현
-    public void play() {
-        myOb.onClick();
-    }
-    public void setListener(Observer o) {
-        this.myOb = o;
-    }
 }
 
-interface Publisher {
-    public void addObserver(Observer o);
-
-    public void deleteObserver(Observer o);
-
-    public void notifyObservers();
+//관찰자 객체 인터페이스
+interface Observer {
+    //이벤트 발생에 따른 행위
+    public void notify(boolean play);
 }
 
-class MyClassA implements Observer {
+//관찰자 객체 구현
+class ObserverA implements Observer {
     private boolean bPlay;
     private Publisher publisher;
 
-    public MyClassA(Publisher publisher) {
+    public ObserverA(Publisher publisher) {
         this.publisher = publisher;
         publisher.addObserver(this);
     }
 
     @Override
-    public void update(boolean play) {
+    public void notify(boolean play) {
         this.bPlay = play;
         myActControl();
-    }
-
-    @Override
-    public void onClick() {
-
     }
 
     public void myActControl() {
@@ -115,24 +79,18 @@ class MyClassA implements Observer {
     }
 }
 
-class MyClassB implements Observer {
+///관찰자 객체 구현
+class ObserverB implements Observer {
     private boolean bPlay;
-    private Publisher publisher;
 
-    public MyClassB(Publisher publisher) {
-        this.publisher = publisher;
+    public ObserverB(Publisher publisher) {
         publisher.addObserver(this);
     }
 
     @Override
-    public void update(boolean play) {
+    public void notify(boolean play) {
         this.bPlay = play;
         myActControl();
-    }
-
-    @Override
-    public void onClick() {
-
     }
 
     public void myActControl() {
@@ -141,5 +99,54 @@ class MyClassB implements Observer {
         } else {
             System.out.println("MyClassB : 동작을 정지합니다.");
         }
+    }
+}
+
+//main 테스트
+public class ObserverTest {
+    public static void main(String[] args) {
+        PlayController playController = new PlayController();
+        Observer ob1 = new ObserverA(playController);
+        Observer ob2 = new ObserverB(playController);
+
+        playController.setFlag(true);
+
+        playController.deleteObserver(ob1);
+        playController.setFlag(false);
+
+
+
+        Button button1 = new Button();
+        button1.setButtonListener(() -> {
+            System.out.println("버튼 클릭");
+        });
+
+        button1.notifyButton();
+    }
+}
+
+
+
+interface ButtonPublisher {
+    public void setButtonListener(Button.OnClickListener listener);
+    public void notifyButton();
+}
+
+
+class Button implements ButtonPublisher {
+    OnClickListener listener;
+
+    @Override
+    public void setButtonListener(OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void notifyButton() {
+        listener.Onclick();
+    }
+
+    interface OnClickListener {
+        void Onclick();
     }
 }
