1283e8ed1379863b83616078a3770b0cbc417bd9
CJW23 <cjw7242@gmail.com>
2020-12-24 22:32:21 +0900

    스프링 시큐리티 테스트2

--- Inflearn_Spring_Security/src/test/java/com/cjw/security/account/AccountControllerTest.java
+++ Inflearn_Spring_Security/src/test/java/com/cjw/security/account/AccountControllerTest.java
@@ -1,5 +1,7 @@
 package com.cjw.security.account;
 
+import com.cjw.security.entity.Account;
+import com.cjw.security.service.AccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +28,13 @@ public class AccountControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    AccountService accountService;
+
+    /**
+     * 로그인 하지 않은 사용자 테스트
+     * @throws Exception
+     */
     @Test
     public void index_anonymous() throws Exception {
         mockMvc.perform(get("/").with(anonymous())) //로그인 하지 않은 사용자
@@ -29,6 +42,10 @@ public class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
+    /**
+     * USER 권한 테스트
+     * @throws Exception
+     */
     @Test
     @WithMockUser(username = "cjw", roles = "USER")
     public void index_user() throws Exception {
@@ -38,10 +55,46 @@ public class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
+    /**
+     * ADMIN 권한 테스트
+     * @throws Exception
+     */
     @Test
     public void admin_user() throws Exception {
         mockMvc.perform(get("/admin").with(user("cjw").roles("USER")))
                 .andDo(print())
                 .andExpect(status().isForbidden());
+    }
+
+    /**
+     * 로그인 성공 테스트
+     * @throws Exception
+     */
+    @Test
+    @Transactional  //테스트 메소드 실행후 롤백
+    public void login_success() throws Exception {
+        Account user = this.createUser("a", "a");
+        mockMvc.perform(formLogin().user(user.getUsername()).password("a"))
+                .andExpect(authenticated());
+    }
+
+    /**
+     * 로그인 실패 테스트
+     * @throws Exception
+     */
+    @Test
+    @Transactional
+    public void login_fail() throws Exception {
+        Account user = this.createUser("a", "a");
+        mockMvc.perform(formLogin().user(user.getUsername()).password("ab"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("ADMIN");
+        return accountService.createNew(account);
     }
 }
